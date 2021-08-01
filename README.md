50bd5589e45dfa22e3aa6853a5b191d0eae8ea82
Ari Fordsham <arifordsham@gmail.com>
2021-08-01 12:36:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# anagrams
+
+```
+anagrams <word>
+```
+
+An anagram finder I wrote in about twenty minutes for a friend.
---
+++ anagrams.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE TupleSections #-}
+
+import System.Environment
+
+import Data.Char
+
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+
+wordMap s =
+    (
+        Map.fromSet (\c -> (length . filter (==c)) s) .
+        Set.fromList .
+        fmap toLower
+        ) s
+
+matches m w = Map.isSubmapOfBy (<=) m (wordMap w)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    dict <- readFile "/usr/share/dict/american-english"
+    
+    let wds = fmap (\s -> (s, wordMap s)) (lines dict)
+    let ms = filter (\(_, m) -> matches m (fmap toLower (head (args)))) wds
+
+    putStrLn (unlines (map fst ms))
